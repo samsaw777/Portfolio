23885e868cd532a93e22b7a68360610643f7e65e
Sameep <sameepsawant10@gmail.com>
2022-02-03 20:13:06 +0530

    animation adding

---
+++ src/components/Aboutus.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import MyPhoto from "../Images/image.jpg";
+import { icons } from "../utils/icons";
+
+function Aboutus({ setAboutme }: any) {
+  return (
+    <div className="mt-32" id="about">
+      <div className="flex">
+        <span className="text-2xl font-black text-white">
+          <span className="text-greenTextColor mr-2">1.</span>About Me
+        </span>
+        <div className="border-t-2 border-secondColor w-16 2xl:w-72 xl:w-72 lg:w-72 md:w-72 sm:w-32 h-1 my-auto ml-3 border-opacity-20"></div>
+      </div>
+      <div className="relative flex 2xl:flex-row xl:flex-row lg:flex-row  flex-col-reverse">
+        <div className="2xl:w-3/5 xl:w-3/5 lg:w-3/5 w-full mt-4">
+          <p className="text-xl pb-4 font-bold">
+            Hii, My name is Sameep and I enjoy creating things that will be
+            useful for other on the internet. My interest in web development
+            started back in 2018 when I tried to clone some templates available
+            on internet.
+          </p>
+          <p className="text-xl pb-4 font-bold">
+            I also recently developed{" "}
+            <span className="text-greenTextColor font-bold">
+              Web Application
+            </span>{" "}
+            that covers cool feature like Chating, Sharing and much more.
+          </p>
+          <p className="text-lg">
+            Here are a few technologies I've been working with recently.
+          </p>
+          <div className="grid grid-cols-2 mt-3 text-center">
+            {icons.map((icon: any, key: number) => (
+              <div className="flex mb-2" key={icon.url}>
+                <img src={icon.url} alt={icon.desc} className="w-5 h-5" />
+                <span className="font-bold ml-2 text-sm">{icon.desc}</span>
+              </div>
+            ))}
+            <div className="flex">
+              <span
+                className="iconify tailwind"
+                data-icon="simple-icons:tailwindcss"
+                data-inline="false"
+              ></span>
+              <span>Tailwind CSS</span>
+            </div>
+          </div>
+        </div>
+
+        <img
+          src={MyPhoto}
+          className="w-72 h-72 rounded-full mx-auto mt-10"
+          alt="Sameep Sawant"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Aboutus;
---
+++ src/components/Introduction.tsx
@@ -0,0 +1,194 @@
+import React from "react";
+
+// import Resume from "./Resume.pdf";
+import Resume from "./Resume.pdf";
+import { motion, Variants } from "framer-motion";
+
+const fadeDown: Variants = {
+  initial: {
+    y: -60,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const fadeSide = (direction: "left" | "right" = "right"): Variants => {
+  return {
+    initial: {
+      x: direction === "right" ? -60 : 60,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+
+      transition: {
+        duration: 0.5,
+        ease: "easeInOut",
+      },
+    },
+  };
+};
+const staggerContainer = {
+  initial: {},
+  animate: {
+    transition: {
+      staggerChildren: 0.4,
+      delayChildren: 0.1,
+    },
+  },
+};
+
+function Introduction() {
+  const Introduction = "Hii,";
+  const Name = "I'm Sameep Sawant";
+  const Profession = "A full stack web developer";
+
+  const spanVariants: Variants = {
+    initial: { y: 0, scaleY: 1 },
+    hover: {
+      y: [-1, -2, -2.8, 0.9, 0],
+      scaleY: [1, 1.3, 0.8, 1, 1.2],
+      color: "red",
+    },
+  };
+
+  const list: Variants = {
+    visible: {
+      opacity: 1,
+      transition: { when: "beforeChildre", staggerChildren: 0.2 },
+    },
+    hidden: {
+      opacity: 0,
+      transition: {
+        when: "afterChildren",
+      },
+    },
+  };
+
+  const items = {
+    visible: {
+      opacity: 1,
+      y: [-1, -1.9, -2.7, 1],
+      scaleY: [1, 1.3, 0.8, 1],
+    },
+    hidden: {
+      opacity: 0,
+    },
+  };
+
+  return (
+    <>
+      {/* <p
+        className="text-greenTextColor mb-2 pl-2 text-xl"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+      >
+        Hello, my name is
+      </p>
+      <h1
+        className="text-gray-100  2xl:text-landingSize xl:text-landingSize lg:text-landingSize md:text-mediumSize p-0 m-0 text-mobileSize font-black"
+        data-aos="fade-up"
+        data-aos-duration="1500"
+      >
+        Sameep Anil Sawant.
+      </h1>
+      <h1
+        className="text-secondColor 2xl:text-landingSize xl:text-landingSize lg:text-landingSize md:text-mediumSize p-0 m-0 text-mobileSize font-black"
+        data-aos="fade-up"
+        data-aos-duration="2000"
+      >
+        I build Applications for web.
+      </h1>
+      <p
+        className="text-secondColor 2xl:w-2/5 xl:w-2/5 lg:w-2/5 md:w-2/3 text-xl mt-2 text-justify w-full"
+        data-aos="fade-up"
+        data-aos-duration="2500"
+      >
+        I a'm a software engineering student. I like to design and develope
+        Website and WebApp(using React) and also like to make them look more
+        attractive.
+      </p> */}
+      <div className="flex flex-col">
+        <motion.span
+          className="cursor-pointer"
+          variants={staggerContainer}
+          initial="initial"
+          animate="animate"
+        >
+          {Introduction.split("").map((letter: string, key: number) => {
+            return (
+              <motion.span
+                key={key}
+                variants={fadeSide("left")}
+                className="text-3xl font-bold"
+              >
+                {letter}
+              </motion.span>
+            );
+          })}
+        </motion.span>
+        {/* <span className="cursor-pointer">
+          {Name.split("").map((letter: string, key: number) => {
+            if (letter !== "S") {
+              return (
+                <motion.span
+                  key={key}
+                  variants={spanVariants}
+                  initial="intial"
+                  whileHover="hover"
+                  className="text-gray-100 2xl:text-landingSize xl:text-landingSize lg:text-landingSize md:text-mediumSize p-0 m-0 text-mobileSize font-black"
+                >
+                  {letter}
+                </motion.span>
+              );
+            } else {
+              return (
+                <motion.span
+                  key={key}
+                  className="text-red-500 2xl:text-landingSize xl:text-landingSize lg:text-landingSize md:text-mediumSize p-0 m-0 text-mobileSize font-black"
+                >
+                  {letter}
+                </motion.span>
+              );
+            }
+          })}
+        </span>
+        <span className="cursor-pointer">
+          {Profession.split("").map((letter: string, key: number) => {
+            return (
+              <motion.span
+                key={key}
+                variants={spanVariants}
+                initial="visible"
+                whileHover="hover"
+                className="text-3xl font-bold"
+              >
+                {letter}
+              </motion.span>
+            );
+          })}
+        </span> */}
+      </div>
+      <div className="w-full mt-16" data-aos="fade-up" data-aos-duration="3000">
+        <a
+          href={Resume}
+          download
+          className="w-42 mt-5 p-3 buttonCss text-greenTextColor rounded text-lg hover:bg-greenTextColor hover:bg-opacity-20"
+        >
+          Download Resume
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Introduction;
