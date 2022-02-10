01bde764acec1cfe8453f6bac63b171b30b6d4ba
Sameep <sameepsawant10@gmail.com>
2022-02-10 10:21:14 +0530

    side icons animation added

--- src/components/LinkIcons.tsx
+++ src/components/LinkIcons.tsx
@@ -1,9 +1,62 @@
 import React from "react";
+import { motion, Variants } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+//icons array.
+const icons = [
+  {
+    name: "LinkedIn",
+    className: "fa fa-linkedin-square",
+    href: "https://www.linkedin.com/in/sameep-sawant/",
+  },
+  {
+    name: "Github",
+    className: "fa fa-github",
+    href: "https://github.com/samsaw777",
+  },
+  {
+    name: "Twitter",
+    className: "fa fa-twitter",
+    href: "https://twitter.com/SameepSawant?s=08",
+  },
+  {
+    name: "Instagram",
+    className: "fa fa-instagram",
+    href: "https://www.instagram.com/_samsaw_777/",
+  },
+];
 
+const fadeIn = (direction: "up" | "down" = "up"): Variants => {
+  return {
+    initial: {
+      y: direction === "up" ? 40 : -60,
+      opacity: 0,
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+
+      transition: {
+        duration: 0.5,
+        // ease: "easeInOut",
+      },
+    },
+  };
+};
+
+const staggerContainer: Variants = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+      delayChildren: 0.2,
+    },
+  },
+};
 const LinkIcons = () => {
   return (
-    <div className="w-sideWidth  2xl:flex xl:flex lg:flex md:flex flex-col justify-end text-center hidden">
-      <a
+    <div className="w-sideWidth   md:block text-center hidden">
+      {/* <a
         href="https://www.linkedin.com/in/sameep-sawant/"
         className="text-2xl mb-2 text-secondColor"
       >
@@ -26,8 +79,30 @@ const LinkIcons = () => {
         className="text-2xl mb-2 text-secondColor"
       >
         <i className="fa fa-instagram" aria-hidden="true"></i>
-      </a>
-      <div className="h-32 border-r-2 border-secondColor w-3/6"></div>
+      </a> */}
+      <motion.div
+        variants={staggerContainer}
+        initial="initial"
+        animate="animate"
+        className="flex flex-col h-iconsHeight justify-end"
+      >
+        {icons.map((icon: any, key: any) => (
+          <motion.a
+            href={icon.href}
+            variants={fadeIn("down")}
+            className="text-2xl mb-2 text-secondColor"
+            key={icon.name}
+          >
+            <motion.i className={icon.className} aria-hidden="true"></motion.i>
+          </motion.a>
+        ))}
+      </motion.div>
+      <motion.div
+        variants={fadeIn("up")}
+        animate="animate"
+        initial="initial"
+        className="h-32  border-r-2 border-secondColor w-3/6"
+      ></motion.div>
     </div>
   );
 };
